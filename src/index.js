131b09685ee84c3e0f922e408270db417a7f6f0b
Nikita M <morevnsw@gmail.com>
2020-02-17 13:30:59 +0300

    Added css, js optimization depending on mode. Added extraction styles to external css.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import * as $ from 'jquery';
 import Post from '@models/Post';
 import '@/styles/styles.css';
-import json from './assets/json.json';
+import * as $ from 'jquery';
+// import json from './assets/json.json';
 import WebpackLogo from './assets/image.png';
-import xml from './assets/xml.xml';
-import csv from './assets/csv.csv';
+// import xml from './assets/xml.xml';
+// import csv from './assets/csv.csv';
 
 const post = new Post("Webpack", WebpackLogo);
 // console.log(json);
--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,30 @@ const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniExtractCssPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsWebpackPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+
+const isDev = (process.env.NODE_ENV === 'development');
+const isProd = !isDev;
+
+const optimiztion = () => {
+  const config = {
+    splitChunks: {
+      chunks: 'all'
+    }
+  }
+  
+  if (isProd) {
+    config.minimizer = [
+      new OptimizeCssAssetsWebpackPlugin(),
+      new TerserWebpackPlugin(),
+    ]
+  }
+  
+  return config;
+}
+
 
 module.exports = {
   mode: 'development',
@@ -11,27 +35,26 @@ module.exports = {
     analitics: './analitics.js',
   },
   output: {
-    filename: '[name].[contenthash].js',
+    filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist')
   },
   resolve: {
     extensions: ['.js', '.json', '.png'],
     alias: {
-      '@models': path.resolve(__dirname, 'src/models'),
-      '@': path.resolve(__dirname, 'src'),
+      '@models': './models',//path.resolve(__dirname, 'src/models'),
+      '@': './'//path.resolve(__dirname, 'src'),
     }
   },
-  optimization: {
-    splitChunks: {
-      chunks: 'all'
-    }
-  },
+  optimization: optimiztion(),
   devServer: {
     port: 4200
   },
   plugins: [
     new HTMLWebpackPlugin({
       template: './index.html',
+      minify: {
+        collapseWhitespace: isProd,
+      },
     }),
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin([
@@ -39,13 +62,25 @@ module.exports = {
         from: path.resolve(__dirname, 'src/assets/favicon.ico'),
         to: path.resolve(__dirname, 'dist'),
       }
-    ])
+    ]),
+    new MiniExtractCssPlugin({
+      filename: '[name].[hash].css'
+    })
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: [
+          {
+            loader: MiniExtractCssPlugin.loader,
+            options: {
+              hmr: isDev,
+              reloadAll: true
+            }
+          },
+          'css-loader'
+        ]
       },
       {
         test: /.(png|jpg|svg|giv)$/,
